c69bc8121eed1a1c1300694055a9bbe3fcbc02b4
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-04 00:51:57 +0100

    Add title.js for AnimeDetail.html

---
+++ src/details/title.js
@@ -0,0 +1,18 @@
+const display = document.querySelector('#title')
+const url = 'http://localhost:8081/GetDetailTitle'
+
+fetch(url)
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(data => {
+        data.forEach(anime => {
+            const title =
+                '<li class="list-group-item">' +
+                '<a href ="http://localhost:63342/Bazy_Danych/AnimeDetail.html">' +
+                anime.title +
+                '</a>' +
+                '</li>'
+            display.insertAdjacentHTML("beforeend", title)
+        })
+    })
+    .catch(err => console.log(err)) //to file
