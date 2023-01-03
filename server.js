0806a572020ecbabcca942b412b7f061df4f45b3
Jakub Kryca <72783966+LeMeTwo@users.noreply.github.com>
2023-01-03 23:56:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -62,3 +62,15 @@ app.get('/GetAllAnime',  async function (req, res) {
     res.status(200).json(JSON.parse(JSON.stringify(jresponse)))
 
   })
+
+app.post('/GetAnimeName', async function(req, res) {
+
+  const body = req.body;
+  console.log(body);
+  var result = (await clientA.query("SELECT * FROM anime WHERE title = " + body.title + ";"))
+  console.log("/GetAnimeName");
+  jresponse = result.rows;
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(JSON.parse(JSON.stringify(jresponse)))
+
+})
