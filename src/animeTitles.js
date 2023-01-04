f145c4672baa98d5a42233a454ddf3822bcc206b
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-04 16:18:59 +0100

    Update animeTitles.js

--- src/animeTitles.js
+++ src/animeTitles.js
@@ -8,7 +8,8 @@ fetch(url)
         data.forEach(anime => {
             const title =
                 '<li class="list-group-item">' +
-                '<a href ="http://localhost:63342/Bazy_Danych/AnimeDetail.html">' +
+                '<a href ="http://localhost:63343/Bazy_Danych/AnimeDetail.html" ' +
+                'class="text-secondary">' +
                 anime.title +
                 '</a>' +
                 '</li>'
@@ -16,3 +17,21 @@ fetch(url)
         })
     })
     .catch(err => console.log(err)) //to file
+
+// async function postData(data ={}, suffix) {
+//     let postUrl = 'http://localhost:8081/' + suffix;
+//     return await fetch(postUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     });
+// }
+//
+// const data = {title: "Toradora!"}
+//
+// postData(data, 'GetAnimeName')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
