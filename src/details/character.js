814d7bd58dc1e06929cbe0118ad9f06c2d0b317b
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-04 01:18:59 +0100

    Add character.js

---
+++ src/details/character.js
@@ -0,0 +1,18 @@
+const characterDisplay = document.querySelector('#character')
+const characterUrl = 'http://localhost:8081/GetDetailCharacter'
+
+fetch(characterUrl)
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(data => {
+        data.forEach(character => {
+            const name =
+                '<li class="list-group-item">' +
+                '<a href ="http://localhost:63342/Bazy_Danych/CharacterDetail.html">' +
+                character.name +
+                '</a>' +
+                '</li>'
+            characterDisplay.insertAdjacentHTML("beforeend", name)
+        })
+    })
+    .catch(err => console.log(err)) //to file
