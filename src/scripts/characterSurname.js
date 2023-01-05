0e6c5fd07e0c45330cc93274dd55dbf60ebeb665
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-06 00:01:46 +0100

    Add characterSurname.js

---
+++ src/scripts/characterSurname.js
@@ -0,0 +1,11 @@
+$().ready(function () {
+    const characterSurnameUrl = 'http://localhost:8081/GetCharacterSurname'
+    fetch(characterSurnameUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(character => {
+                $('#surname').append(isNull(character.surname))
+            })
+        })
+        .catch(err => console.log(err)) //to file
+})
