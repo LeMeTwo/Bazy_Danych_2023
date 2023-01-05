5daee2da562372f73ac5bb8e1c6a0ebd67b6a021
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-06 00:01:57 +0100

    Add characterSex.js

---
+++ src/scripts/characterSex.js
@@ -0,0 +1,11 @@
+$().ready(function () {
+    const characterSexUrl = 'http://localhost:8081/GetCharacterSex'
+    fetch(characterSexUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(character => {
+                $('#sex').append(isNull(character.sex))
+            })
+        })
+        .catch(err => console.log(err)) //to file
+})
