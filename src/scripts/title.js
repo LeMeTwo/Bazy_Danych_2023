f12cb570ac693e7b6275ff67559bca8154442f74
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-05 12:33:12 +0100

    Add jQuery, send good json

--- src/scripts/title.js
+++ src/scripts/title.js
@@ -1,17 +1,26 @@
-const titleDisplay = document.querySelector('#title')
-const titleUrl = 'http://localhost:8081/GetDetailTitle'
-
-fetch(titleUrl)
-    .then(response => response.json())
-    //.then(data => console.log(data))
-    .then(data => {
-        data.forEach(anime => {
-            const title =
-                '<a href ="http://localhost:63342/Bazy_Danych/src/AnimeDetail.html" ' +
-                'class="text-secondary">' +
-                anime.title +
-                '</a>'
-            titleDisplay.insertAdjacentHTML("beforeend", title)
+$().ready(function (){
+    const titleUrl = 'http://localhost:8081/GetDetailTitle'
+    fetch(titleUrl)
+        .then(response => response.json())
+        //.then(data => console.log(data))
+        .then(data => {
+            data.forEach(anime => {
+                const title =
+                    '<a href ="http://localhost:63342/Bazy_Danych/src/AnimeDetail.html" ' +
+                    'class="text-secondary">' +
+                    anime.title +
+                    '</a>'
+                $('#title').append(title)
+            })
         })
-    })
-    .catch(err => console.log(err)) //to file
+        .catch(err => console.log(err)) //to file
+
+    $("#title").click(function()
+    {
+        let data = {}
+        data.title = $(this).text();
+        postData(data, 'GetAnimeTest')
+            .then(response => response.json())
+            .then(data => alert(data))
+    });
+})
