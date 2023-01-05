b92585cdc256bcd280600650189cd6532f8cb5fc
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-05 17:21:14 +0100

    Add almost perfect json sending method - send '{cid}'

--- src/scripts/characterList.js
+++ src/scripts/characterList.js
@@ -1,4 +1,4 @@
-$().ready(function (){
+$().ready(function () {
     const characterListUrl = 'http://localhost:8081/GetDetailCharacterList'
     fetch(characterListUrl)
         .then(response => response.json())
@@ -8,22 +8,20 @@ $().ready(function (){
                 const id = character.cid[0]
                 const name =
                     '<li class="list-group-item">' +
-                    '<p style="display: none">' + "[ " + character.cid + " " + '</p>' +
+                    '<p style="display: none">' + character.cid + " " + '</p>' +
                     '<a href ="http://localhost:63342/Bazy_Danych/src/CharacterDetail.html" ' +
                     'class="text-secondary id=' + id + '">' +
                     character.name +
                     '</a>' +
-                    '<p style="display: none">' + " ]" + '</p>' +
                     '</li>'
                 $('#characterList').append(name)
             })
         })
         .catch(err => console.log(err)) //to file
 
-    $("#characterList").click(function()
-    {
+    $("#characterList").on("click", "li",function () {
         let data = {}
-        data.title = $(this).text();
+        data.aid = getNumber($(this).text())
         postData(data, 'GetAnimeTest')
             .then(response => response.json())
             .then(data => alert(data))
