a99704a271223ce583955601ab63c0656b142563
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-05 12:59:59 +0100

    Add jQuery, send better json

---
+++ src/scripts/characterList.js
@@ -0,0 +1,28 @@
+$().ready(function (){
+    const characterUrl = 'http://localhost:8081/GetDetailCharacterList'
+    fetch(characterUrl)
+        .then(response => response.json())
+        //.then(data => console.log(data))
+        .then(data => {
+            data.forEach(character => {
+                const name =
+                    '<li class="list-group-item">' +
+                    '<a href ="http://localhost:63342/Bazy_Danych/src/CharacterDetail.html" ' +
+                    'class="text-secondary">' +
+                    character.name +
+                    '</a>' +
+                    '</li>'
+                $('#characterList').append(name)
+            })
+        })
+        .catch(err => console.log(err)) //to file
+
+    $("#characterList").click(function()
+    {
+        let data = {}
+        data.title = $(this).text();
+        postData(data, 'GetAnimeTest')
+            .then(response => response.json())
+            .then(data => alert(data))
+    });
+})
