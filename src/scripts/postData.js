6e33e20378092ea8c0850fd930c7a619fb2d3f62
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-05 15:34:49 +0100

    Add new function

--- src/scripts/postData.js
+++ src/scripts/postData.js
@@ -1,12 +1,16 @@
 async function postData(data = {}, suffix) {
     let url = 'http://127.0.0.1:8081/' + suffix;
-    const response = await fetch(url, {
+    return await fetch(url, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-    });
-    return response; // parses JSON response into native JavaScript objects
+    }); // parses JSON response into native JavaScript objects
+}
+
+function getNumber(text) {
+    let words = text.split(" ")
+    return "{" + words[0] + "}";
 }
