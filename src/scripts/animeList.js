3f17d6da91079cb782904f775d4f1497a3731588
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-05 17:21:01 +0100

    Add almost perfect json sending method - send '{aid}'

--- src/scripts/animeList.js
+++ src/scripts/animeList.js
@@ -1,4 +1,4 @@
-$().ready(function (){
+$().ready(function () {
     const titleListUrl = 'http://localhost:8081/GetAnimeList'
     fetch(titleListUrl)
         .then(response => response.json())
@@ -8,22 +8,20 @@ $().ready(function (){
                 const id = anime.aid[0]
                 const title =
                     '<li class="list-group-item">' +
-                    '<p style="display: none">' + "[ " + anime.aid + " " + '</p>' +
+                    '<p style="display: none">' + anime.aid + " " + '</p>' +
                     '<a href ="http://localhost:63342/Bazy_Danych/src/AnimeDetail.html" ' +
                     'class="text-secondary id=' + id + '">' +
                     anime.title +
                     '</a>' +
-                    '<p style="display: none">' + " ]" + '</p>' +
                     '</li>'
                 $('#animeList').append(title)
             })
         })
         .catch(err => console.log(err)) //to file
 
-    $("#animeList").click(function()
-    {
+    $("#animeList").on("click", "li",function () {
         let data = {}
-        data.title = $(this).text();
+        data.aid = getNumber($(this).text())
         postData(data, 'GetAnimeTest')
             .then(response => response.json())
             .then(data => alert(data))
