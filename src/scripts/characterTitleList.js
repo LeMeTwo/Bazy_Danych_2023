01180846849d79c1651e0e3bff9de0525898b247
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-05 20:58:20 +0100

    Add characterTitleList.js - method used to deploy clickable titles in which the selected character appears

---
+++ src/scripts/characterTitleList.js
@@ -0,0 +1,29 @@
+$().ready(function () {
+    const characterTitleListUrl = 'http://localhost:8081/GetCharacterTitleList'
+    fetch(characterTitleListUrl)
+        .then(response => response.json())
+        //.then(data => console.log(data))
+        .then(data => {
+            data.forEach(anime => {
+                const id = anime.aid[0]
+                const title =
+                    '<li class="list-group-item">' +
+                    '<p style="display: none">' + anime.aid + " " + '</p>' +
+                    '<a href ="http://localhost:63342/Bazy_Danych/src/AnimeDetail.html" ' +
+                    'class="text-secondary id=' + id + '">' +
+                    anime.title +
+                    '</a>' +
+                    '</li>'
+                $('#characterTitleList').append(title)
+            })
+        })
+        .catch(err => console.log(err)) //to file
+
+    $("#characterTitleList").on("click", "li",function () {
+        let data = {}
+        data.aid = getNumber($(this).text())
+        postData(data, 'PostAnimeId')
+            .then(response => response.json())
+            .then(data => alert(data))
+    });
+})
