36a5957453a6a60af57113238e03727e33cc2791
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-06 02:14:06 +0100

    Create characterVoiceActor.js

---
+++ src/scripts/characterVoiceActor.js
@@ -0,0 +1,26 @@
+$().ready(function () {
+    const titleUrl = 'http://localhost:8081/GetCharacterVoiceActor'
+    fetch(titleUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(voiceActor => {
+                const id = voiceActor.vid[0]
+                const name =
+                    '<p style="display: none">' + voiceActor.vid + " " + '</p>' +
+                    '<a href ="http://localhost:63342/Bazy_Danych/src/VoiceActorDetail.html" ' +
+                    'class="text-secondary text-truncate id=' + id + '">' +
+                    isNullComma(voiceActor.surname) + isNull(voiceActor.name) +
+                    '</a>'
+                $('#voiceActor').append(name)
+            })
+        })
+        .catch(err => console.log(err)) //to file
+
+    $("#voiceActor").click(function() {
+        let data = {}
+        data.vid = getNumber($(this).text())
+        postData(data, 'PostAnimeTest')
+            .then(response => response.json())
+            .then(data => alert(data))
+    });
+})
