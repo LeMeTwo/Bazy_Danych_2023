ef3a646cbb1afb61bf38fdefe5f899f4e19128fa
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-07 21:02:43 +0100

    Create voiceActorSurname.js

---
+++ src/scripts/voiceActorSurname.js
@@ -0,0 +1,11 @@
+$().ready(function () {
+    const voiceActorSurnameUrl = 'http://localhost:8081/GetVoiceActorSurname'
+    fetch(voiceActorSurnameUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(actor => {
+                $('#actorSurname').append(isNull(actor.surname))
+            })
+        })
+        .catch(err => console.log(err)) //to file
+})
