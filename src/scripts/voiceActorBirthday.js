9297fa3c3f888862a8b26a36b401e90362ab7fca
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-07 21:03:12 +0100

    Create voiceActorBirthday.js

---
+++ src/scripts/voiceActorBirthday.js
@@ -0,0 +1,11 @@
+$().ready(function () {
+    const voiceActorBirthdayUrl = 'http://localhost:8081/GetVoiceActorBirthday'
+    fetch(voiceActorBirthdayUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(actor => {
+                $('#actorBirthday').append(isNull(getDate(actor.birth)))
+            })
+        })
+        .catch(err => console.log(err)) //to file
+})
