e744f06dca3caf1cdbcd2da3cd3e889f650c397b
Ablesh1 <michal.ablewski19@gmail.com>
2023-01-09 01:48:12 +0100

    Add findStringByKey function and do some refactoring

--- src/scripts/methods.js
+++ src/scripts/methods.js
@@ -10,15 +10,29 @@ async function postData(data = {}, suffix) {
     }); // parses JSON response into native JavaScript objects
 }
 
-function getNumber(text) {
-    let words = text.split(" ")
-    return "{" + words[0] + "}";
+function isNull(variable) {
+    if(variable == null) {return ""} else {return variable}
 }
 
+function isNullComma(variable) {
+    if(variable == null) {return ""} else {return variable + ", "}
+}
+
+// Used to return id inside {} block
+function getNumber(text) {
+    if (text == null) {return ""}
+    else {
+        let words = text.split(" ")
+        return "{" + words[0] + "}";
+    }
+}
+
+// Used in voiceActorHome.js to return city separated from the country
 function getArray(array) {
     if(array == null) {return ""} else {return array.join(", ")}
 }
 
+// Used in voiceActorBirthday.js to return date without T-time
 function getDate(string) {
     if (string == null) {return ""}
     else {
@@ -27,10 +41,18 @@ function getDate(string) {
     }
 }
 
-function isNull(variable) {
-    if(variable == null) {return ""} else {return variable}
-}
-
-function isNullComma(variable) {
-    if(variable == null) {return ""} else {return variable + ", "}
+// Used in addTags.js to prepare id arrays for jsons
+function findStringByKey(selectedArray, idArray, key, keyLength) {
+    let here
+    let index = selectedArray.findIndex(element => {
+        if(element.includes(key)) {
+            here = true
+            return true;
+        }
+    });
+    if(here) {
+        idArray.push(selectedArray[index].substring(keyLength))
+        selectedArray.splice(index, 1)
+        here = false
+    }
 }
