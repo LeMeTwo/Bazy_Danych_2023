7b43798219f59082a70077125c03c426fc6ea989
Ablesh1 <michal.ablewski19@gmail.com>
2023-02-05 00:14:03 +0100

    Change id
    Signed-off-by: Ablesh1 <michal.ablewski19@gmail.com>

--- src/scripts/anime_detail/animeCharacterList.js
+++ src/scripts/anime_detail/animeCharacterList.js
@@ -14,12 +14,12 @@ $().ready(function () {
 					isNullComma(character.surname) + isNull(character.name) +
 					'</a>' +
 					'</li>';
-				$('#characterList').append(name);
+				$('#animeCharacterList').append(name);
 			});
 		})
 		.catch(err => console.log(err)); //to file
 
-	$('#characterList').on('click', 'li', function () {
+	$('#animeCharacterList').on('click', 'li', function () {
 		let data = {};
 		data.cid = getNumber($(this).text());
 		postData(data, 'PostCharacterID')
